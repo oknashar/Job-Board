23999d9619ac228b080390ba338b45a63977e39a
Omar khalid <oknashar@gmail.com>
2020-07-19 12:24:19 +0200

    job views and templates

--- job/views.py
+++ job/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
+
+def job_list(request):
+    jobList = Job.objects.all()
+
+    context = {
+        'jobs':jobList
+    }
+    return render(request,'job_list.html',context)
+
+def job_detail(request,id):
+    job_det = Job.objects.get(id=id)
+    context = {'job':job_det}
+    return render(request,'job_detail.html',context)
